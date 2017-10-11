f2c12dfbbb456b9ab9d1e787d5f75e4e12b58696
github username <github email address>
2017-10-11 19:31:47 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+  require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +9,62 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = self.new
+    self.all << new_song
+    new_song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    self.all << song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.detect {|p| p.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort{|a,b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(filename)
+    array = filename.chomp(".mp3").split(" - ")
+    song = self.new
+    song.name = array[1]
+    song.artist_name = array[0]
+    song
+  end
+
+  def self.create_from_filename(filename)
+    array = filename.chomp(".mp3").split(" - ")
+    song = self.new
+    song.name = array[1]
+    song.artist_name = array[0]
+    self.all << song
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
